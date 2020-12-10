1544fd1ffd26882c9d40b7647df7b9a0ac9c2479
Hélène Baudrand <baudrand.helene@gmail.com>
2020-12-10 13:59:52 +0100

    added tests with Exceptions

--- src/Auth.php
+++ src/Auth.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Exception\ForbiddenException;
 use PDO;
 
 class Auth
@@ -37,10 +38,7 @@ class Auth
 
     public function user(): ?User
     {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-        $id = $_SESSION['auth'] ?? null;
+        $id = $this->session['auth'] ?? null;
         if ($id === null) {
             return null;
         }
@@ -53,9 +51,13 @@ class Auth
     public function requireRole(string ...$roles): void {
 
         $user = $this->user();
-        if ($user === null || !in_array($user->role, $roles)) {
-            header("Location: {$this->loginPath}?forbid=1");
-            exit();
+        if ($user === null) {
+            throw new ForbiddenException("Please login to see this page");
+        }
+        if (!in_array($user->role, $roles)) {
+            $roles = implode(',', $roles);
+            $role = $user->role;
+            throw new ForbiddenException("Your role is $role. Only $roles can access to this page");
         }
     }
 }
--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -22,10 +22,10 @@ class AuthTest extends TestCase
         ]);
 
         // Create and insert values into the fictive db
-        $pdo->query('CREATE TABLE users (id INTEGER, username TEXT, password TEXT)');
+        $pdo->query('CREATE TABLE users (id INTEGER, username TEXT, password TEXT, role TEXT)');
         for ($i = 1; $i <= 10; $i++) {
             $password = password_hash("user$i", PASSWORD_BCRYPT);
-            $pdo->query("INSERT INTO users (id, username, password) VALUES ($i, 'user$i', '$password')");
+            $pdo->query("INSERT INTO users (id, username, password, role) VALUES ($i, 'user$i', '$password', 'user$i')");
         }
         $this->auth = new Auth($pdo, "/login", $this->session);
 
@@ -45,5 +45,44 @@ class AuthTest extends TestCase
     {
         $this->assertObjectHasAttribute("username", $this->auth->login('user1', 'user1'));
         $this->assertEquals(1, $this->session['auth']);
+    }
+
+    public function testUserWhenNotConnected()
+    {
+        $this->assertNull($this->auth->user());
+    }
+
+    public function testUserWhenConnectedWithNotExistingUser()
+    {
+        $this->session['auth'] = 11;
+        $this->assertNull($this->auth->user());
+    }
+
+    public function testUserWhenConnected()
+    {
+        $this->session['auth'] = 2;
+        $user = $this->auth->user();
+        $this->assertIsObject($user);
+        $this->assertEquals("user2", $user->username);
+    }
+
+    public function testRequireRole()
+    {
+        $this->session['auth'] = 2;
+        $this->auth->requireRole('user2');
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testRequireRoleThrowException()
+    {
+        $this->expectException(App\Exception\ForbiddenException::class);
+        $this->session['auth'] = 2;
+        $this->auth->requireRole('user3');
+    }
+
+    public function testRequireRoleWithoutLoginThrowException()
+    {
+        $this->expectException(App\Exception\ForbiddenException::class);
+        $this->auth->requireRole('user3');
     }
 }
